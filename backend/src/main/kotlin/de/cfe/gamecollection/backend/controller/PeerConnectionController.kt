da8be0d062086763bf434007550cee3ecb89bd45
byChratos <byChratos@gmx.de>
2026-07-11 20:26:14 +0200

    Init rtc

---
+++ backend/src/main/kotlin/de/cfe/gamecollection/backend/controller/PeerConnectionController.kt
@@ -0,0 +1,12 @@
+package de.cfe.gamecollection.backend.controller
+
+import org.springframework.stereotype.Component
+
+@Component
+class PeerConnectionController {
+
+    fun send(event: String) {
+        
+    }
+
+}
